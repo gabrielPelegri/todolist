3f430cf376b2f1caf7beed734649140104d4b59e
Gabriel Pelegri <gpelerii@myuax.com>
2022-10-16 20:56:50 +0200

    Añadida la opción de acceder a la descripción de cada usuario a través de un enlace, con su implementación de controller y vista

--- src/main/java/madstodolist/controller/UsuariosController.java
+++ src/main/java/madstodolist/controller/UsuariosController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class UsuariosController {
@@ -16,8 +20,15 @@ public class UsuariosController {
 
     @GetMapping("/registrados")
     public String getUsuarios(Model model) {
-        Iterable<Usuario> usuarios = usuarioService.findAll();
+        List<Usuario> usuarios = usuarioService.findAll();
         model.addAttribute("usuarios", usuarios);
         return "listaUsuarios";
+    }
+
+    @RequestMapping("/registrados/{id}")
+    public String getUsuarios(Model model, @PathVariable(value = "id") Long id){
+       Usuario usuario= usuarioService.findById(id);
+       model.addAttribute("usuario",usuario);
+       return "descripcionUsuario";
     }
 }
